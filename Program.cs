dae4f481be3a820aba90af78317d419735ee66d6
robdesautel <rob.desautel@gmail.com>
2025-02-22 11:00:45 -0500

    Created the bot to be and empty ASP.NET Core Empty project

---
+++ Program.cs
@@ -0,0 +1,15 @@
+namespace Telegram_ToughSociety_Bot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
